192ac0868a2cc66b4ca0a4ea090c4e3b40e6bd75
Michelino Gali <107483586+migelogali@users.noreply.github.com>
2025-08-03 21:12:02 +0000

    Completed fifth section (input+output redirection). Starting sixth section (executing commands in foreground+background)

--- sample_parser.c
+++ sample_parser.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <sys/wait.h> // for status
 #include <unistd.h>  // for execv
+#include <fcntl.h> // for open
 
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
@@ -147,6 +148,38 @@ int main() {
 					exit(1);
 					break;
 				case 0:
+					// handle input file in child process first, then output file
+					if (curr_command->input_file != NULL) {
+						int sourceFD = open(curr_command->input_file, O_RDONLY);
+						if (sourceFD == -1) { 
+							perror("source open()"); 
+							exit(1); 
+						}
+						// Redirect stdin to source file
+						int result = dup2(sourceFD, 0);
+						if (result == -1) { 
+							perror("source dup2()"); 
+							exit(1); 
+						}
+						// otherwise, input file successfully opened, so close it
+						close(sourceFD);
+					}
+					if (curr_command->output_file != NULL) {
+						int targetFD = open(curr_command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+						if (targetFD == -1) { 
+							perror("target open()"); 
+							exit(1);
+						}
+						// Redirect stdout to target file
+						int result = dup2(targetFD, 1);
+						if (result == -1) { 
+							perror("target dup2()"); 
+							exit(1);
+						}
+						// otherwise, output file successfully opened, so close it
+						close(targetFD);
+					}
+
 					// The child process executes this branch
 					printf("CHILD(%d) running command %s\n", getpid(), curr_command->argv[0]);
 					// Replace the current program
